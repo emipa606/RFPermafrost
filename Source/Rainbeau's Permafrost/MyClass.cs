d47cf2481684ac6cf8f0026fe987cd0c8cd73e7d
troopersmith1 <62363949+troopersmith1@users.noreply.github.com>
2020-08-17 01:03:35 -0400

    Add files via upload

--- Source/Rainbeau's Permafrost/MyClass.cs
+++ Source/Rainbeau's Permafrost/MyClass.cs
@@ -67,9 +67,9 @@ namespace RPF_Code {
 		}
 	}		
 
-	public class Watcher : MapComponent {
+	public class Lakes : MapComponent {
 		public int cycleIndex;
-		public Watcher(Map map) : base(map) { }
+		public Lakes(Map map) : base(map) { }
 		public override void MapComponentTick() {
 			base.MapComponentTick();
 			if (Controller.Settings.waterFreezes.Equals(false)) { return; }
@@ -87,7 +87,7 @@ namespace RPF_Code {
 				float permafrostChance = deepChance;
 				if (permafrostChance > 0.2f) { permafrostChance = 0.2f; }
 				if (cellTemp < 0f) {
-					if (terrainDef.defName == "WaterShallow" || terrainDef.defName == "WaterMovingShallow" || terrainDef.defName == "IceST" || terrainDef.defName == "IceSMT") {
+					if (terrainDef.defName == "WaterShallow" || terrainDef.defName == "IceST" ) {
 						int freezable = 0;
 						for (int k = -1; k < 2; k++) {
 							for (int j = -1; j < 2; j++) {
@@ -113,13 +113,11 @@ namespace RPF_Code {
 							}
 							string iceType;
 							if (terrainDef.defName == "WaterShallow") { iceType = "IceST"; }
-							else if (terrainDef.defName == "WaterMovingShallow") { iceType = "IceSMT"; }
-							else if (terrainDef.defName == "IceST") { iceType = "IceS"; }
-							else { iceType = "IceSM"; }
+							else { iceType = "IceS"; }
 							map.terrainGrid.SetTerrain (c, TerrainDef.Named(iceType));
 						}
 					}
-					if (terrainDef.defName == "WaterDeep" || terrainDef.defName == "WaterMovingChestDeep" || terrainDef.defName == "IceDT" || terrainDef.defName == "IceDMT") {
+					if (terrainDef.defName == "WaterDeep" || terrainDef.defName == "IceDT" ) {
 						int freezable = 0;
 						for (int k = -1; k < 2; k++) {
 							for (int j = -1; j < 2; j++) {
@@ -145,9 +143,7 @@ namespace RPF_Code {
 							}
 							string iceType;
 							if (terrainDef.defName == "WaterDeep") { iceType = "IceDT"; }
-							else if (terrainDef.defName == "WaterMovingChestDeep") { iceType = "IceDMT"; }
-							else if (terrainDef.defName == "IceDT") { iceType = "IceD"; }
-							else { iceType = "IceDM"; }
+							else { iceType = "IceD"; }
 							map.terrainGrid.SetTerrain (c, TerrainDef.Named(iceType));
 						}
 					}
@@ -209,8 +205,8 @@ namespace RPF_Code {
 						}
 					}
 				}
-				if (cellTemp > 0f) {
-					if (terrainDef.defName.Contains("IceS") || terrainDef.defName.Contains("IceD")) {
+				if (cellTemp > -4f) {
+					if (terrainDef.defName == "IceS" || terrainDef.defName == "IceD" || terrainDef.defName == "IceST" || terrainDef.defName == "IceDT" ) {
 						int quickThaw = 1;
 						for (int k = -1; k < 2; k++) {
 							for (int j = -1; j < 2; j++) {
@@ -241,11 +237,7 @@ namespace RPF_Code {
 							if (terrainDef.defName == "IceST") { waterType = "WaterShallow"; }
 							else if (terrainDef.defName == "IceS") { waterType = "IceST"; }
 							else if (terrainDef.defName == "IceDT") { waterType = "WaterDeep"; }
-							else if (terrainDef.defName == "IceD") { waterType = "IceDT"; }
-							else if (terrainDef.defName == "IceSMT") { waterType = "WaterMovingShallow"; }
-							else if (terrainDef.defName == "IceSM") { waterType = "IceSMT"; }
-							else if (terrainDef.defName == "IceDMT") { waterType = "WaterMovingChestDeep"; }
-							else { waterType = "IceDMT"; }
+							else { waterType = "IceDT"; }
 							if (waterType.Contains("Water")) {
 								map.snowGrid.SetDepth(c, 0f);
 							}
@@ -313,7 +305,169 @@ namespace RPF_Code {
 		}
 	}
 
-    public class BiomeWorker_Permafrost : BiomeWorker {
+	public class Rivers : MapComponent
+	{
+		public int cycleIndex;
+		public Rivers(Map map) : base(map) { }
+		public override void MapComponentTick()
+		{
+			base.MapComponentTick();
+			if (Controller.Settings.waterFreezes.Equals(false)) { return; }
+			int num = Mathf.RoundToInt((float)this.map.Area * 0.00005f);
+			int area = this.map.Area;
+			float cellTemp;
+			for (int i = 0; i < num; i++)
+			{
+				this.cycleIndex++;
+				if (this.cycleIndex >= area) { this.cycleIndex = 0; }
+				IntVec3 c = this.map.cellsInRandomOrder.Get(this.cycleIndex);
+				var terrainDef = map.terrainGrid.TerrainAt(c);
+				if (!GenTemperature.TryGetTemperatureForCell(c, map, out cellTemp)) { cellTemp = 0f; }
+				float shallowChance = (cellTemp) * (cellTemp) * 0.8f/ 100f;
+				float deepChance = ((cellTemp) * (cellTemp) * 0.16f) / 100f;
+				float permafrostChance = deepChance;
+				if (permafrostChance > 0.2f) { permafrostChance = 0.2f; }
+				if (cellTemp < -8f)
+				{
+					if (terrainDef.defName == "WaterMovingShallow" ||terrainDef.defName == "IceSMT")
+					{
+						int freezable = 0;
+						for (int k = -1; k < 2; k++)
+						{
+							for (int j = -1; j < 2; j++)
+							{
+								int x = c.x + k;
+								int z = c.z + j;
+								if ((k == 0 && j == 1) || (k == 0 && j == -1) || (k == 1 && j == 0) || (k == -1 && j == 0))
+								{
+									if (x > 0 && x < this.map.Size.x && z > 0 && z < this.map.Size.z)
+									{
+										IntVec3 newSpot = new IntVec3(x, 0, z);
+										if (!map.terrainGrid.TerrainAt(newSpot).defName.Contains("Water") && map.terrainGrid.TerrainAt(newSpot).defName != "Marsh" && map.terrainGrid.TerrainAt(newSpot).defName != "BridgeMarsh" && map.terrainGrid.TerrainAt(newSpot).defName != "Bridge")
+										{
+											freezable++;
+										}
+									}
+								}
+							}
+						}
+						if (Rand.Value < shallowChance * freezable)
+						{
+							List<Thing> thingList = c.GetThingList(map);
+							for (int j = 0; j < thingList.Count; j++)
+							{
+								Thing item = thingList[j];
+								if (item.def.defName == "Fishing Spot" || item.def.defName == "ZARS_FishingSpot")
+								{
+									item.Destroy(DestroyMode.Vanish);
+								}
+							}
+							string iceType;
+							if (terrainDef.defName == "WaterMovingShallow") { iceType = "IceSMT"; }
+							else { iceType = "IceSM"; }
+							map.terrainGrid.SetTerrain(c, TerrainDef.Named(iceType));
+						}
+					}
+					if (terrainDef.defName == "WaterMovingChestDeep" || terrainDef.defName == "IceDMT")
+					{
+						int freezable = 0;
+						for (int k = -1; k < 2; k++)
+						{
+							for (int j = -1; j < 2; j++)
+							{
+								int x = c.x + k;
+								int z = c.z + j;
+								if ((k == 0 && j == 1) || (k == 0 && j == -1) || (k == 1 && j == 0) || (k == -1 && j == 0))
+								{
+									if (x > 0 && x < this.map.Size.x && z > 0 && z < this.map.Size.z)
+									{
+										IntVec3 newSpot = new IntVec3(x, 0, z);
+										if (!map.terrainGrid.TerrainAt(newSpot).defName.Contains("Water") && map.terrainGrid.TerrainAt(newSpot).defName != "Marsh" && map.terrainGrid.TerrainAt(newSpot).defName != "BridgeMarsh" && map.terrainGrid.TerrainAt(newSpot).defName != "Bridge")
+										{
+											freezable++;
+										}
+									}
+								}
+							}
+						}
+						if (Rand.Value < deepChance * freezable)
+						{
+							List<Thing> thingList = c.GetThingList(map);
+							for (int j = 0; j < thingList.Count; j++)
+							{
+								Thing item = thingList[j];
+								if (item.def.defName == "Fishing Spot" || item.def.defName == "ZARS_FishingSpot")
+								{
+									item.Destroy(DestroyMode.Vanish);
+								}
+							}
+							string iceType;
+							if (terrainDef.defName == "WaterMovingChestDeep") { iceType = "IceDMT"; }
+							else { iceType = "IceDM"; }
+							map.terrainGrid.SetTerrain(c, TerrainDef.Named(iceType));
+						}
+					}
+				}
+				if (cellTemp > -20f)
+				{
+					if (terrainDef.defName.Contains("IceSM") || terrainDef.defName.Contains("IceDM"))
+					{
+						int quickThaw = 1;
+						for (int k = -1; k < 2; k++)
+						{
+							for (int j = -1; j < 2; j++)
+							{
+								int x = c.x + k;
+								int z = c.z + j;
+								if ((k == 0 && j == 1) || (k == 0 && j == -1) || (k == 1 && j == 0) || (k == -1 && j == 0))
+								{
+									if (x > 0 && x < this.map.Size.x && z > 0 && z < this.map.Size.z)
+									{
+										IntVec3 newSpot = new IntVec3(x, 0, z);
+										if (!terrainDef.label.Contains("Thin"))
+										{
+											if (map.terrainGrid.TerrainAt(newSpot).defName.Contains("Water") || map.terrainGrid.TerrainAt(newSpot).defName == "Marsh" || map.terrainGrid.TerrainAt(newSpot).label.Contains("Thin"))
+											{
+												quickThaw++;
+											}
+										}
+										else
+										{
+											if (map.terrainGrid.TerrainAt(newSpot).defName.Contains("Water") || map.terrainGrid.TerrainAt(newSpot).defName == "Marsh")
+											{
+												quickThaw++;
+											}
+										}
+									}
+								}
+							}
+						}
+						if (quickThaw == 1)
+						{
+							if (Rand.Value < 0.67f) { quickThaw = 0; }
+						}
+						if (Rand.Value < (shallowChance * quickThaw))
+						{
+							string waterType;
+							if (terrainDef.defName == "IceSMT") { waterType = "WaterMovingShallow"; }
+							else if (terrainDef.defName == "IceSM") { waterType = "IceSMT"; }
+							else if (terrainDef.defName == "IceDMT") { waterType = "WaterMovingChestDeep"; }
+							else { waterType = "IceDMT"; }
+							if (waterType.Contains("Water"))
+							{
+								map.snowGrid.SetDepth(c, 0f);
+							}
+							map.terrainGrid.SetTerrain(c, TerrainDef.Named(waterType));
+						}
+					}
+				}
+			}
+		}
+	}
+
+
+
+	public class BiomeWorker_Permafrost : BiomeWorker {
         public BiomeWorker_Permafrost() { }
         public override float GetScore(Tile tile, int tileID) {
             if (tile.WaterCovered) {
